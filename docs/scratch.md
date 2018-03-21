89fa0c16cac8c7f055cc3697ca05385384e9e8f9
Sunny Lam <sunny.lam@gmail.com>
2018-03-21 10:28:02 -0400

    revised the code

--- docs/scratch.md
+++ docs/scratch.md
@@ -18,6 +18,9 @@ test data
 
 	sheet_cell_inverter/updatedProduceSales.xlsx
 
+
+	sheet_cell_inverter/updatedProduceSales_v2.xlsx
+
 should be in same folder as main program
 
 execution code
@@ -25,4 +28,40 @@ execution code
 	python3 sheet_cell_inverter.py pathToSpreadsheet
 
 	python3 sheet_cell_inverter.py updatedProduceSales.xlsx
+
+	python3 sheet_cell_inverter.py updatedProduceSales_v2.xlsx
+
+## Wednesday, March 21, 2018 10:23 AM
+
+It appears there's a limit to the number of rows that can be converted to columns...  
+
+When it's around 18279 rows or after that, conversion to columns results in an error...
+
+Meaning there's a limit to the number of rows that can be inverted...
+
+	2018-03-21 10:24:27,244 - DEBUG - The invert_cell_coordinate is:  ZZZ3
+	 2018-03-21 10:24:27,244 - DEBUG - The invert_column_letter is:  ZZZ
+	 2018-03-21 10:24:27,244 - DEBUG - The invert_cell_coordinate is:  ZZZ4
+	Traceback (most recent call last):
+	  File "/usr/local/lib/python3.6/site-packages/openpyxl/utils/cell.py", line 111, in get_column_letter
+	    return _STRING_COL_CACHE[idx]
+	KeyError: 18279
+
+	During handling of the above exception, another exception occurred:
+
+	Traceback (most recent call last):
+	  File "sheet_cell_inverter.py", line 95, in <module>
+	    row_analyzer(inverted_dict,1,upper_row_max + 1)
+	  File "sheet_cell_inverter.py", line 78, in row_analyzer
+	    invert_column_letter = get_column_letter(rowValue) # the row value becomes the column value so get its letter
+	  File "/usr/local/lib/python3.6/site-packages/openpyxl/utils/cell.py", line 113, in get_column_letter
+	    raise ValueError("Invalid column index {0}".format(idx))
+	ValueError: Invalid column index 18279
+	MacBook-Air:sheet_cell_inverter sunnyair$
+
+In fact `updatedProduceSales.xlsx` has 23758 rows so that means 18279 really is the hard cap as far as I can tell...
+
+That's the limit...
+
+When I use a smaller sheet the program works fine...
 
--- sheet_cell_inverter/sheet_cell_inverter.py
+++ sheet_cell_inverter/sheet_cell_inverter.py
@@ -5,6 +5,7 @@
 # USAGE
 # python3 sheet_cell_inverter.py pathToSpreadsheet
 # python3 sheet_cell_inverter.py updatedProduceSales.xlsx
+# python3 sheet_cell_inverter.py updatedProduceSales_v2.xlsx
 
 import openpyxl, sys
 
@@ -60,7 +61,7 @@ logging.debug('The maximum number of columns in the sheet is %i' % (upper_col_ma
 
 inverted_dict = {} # this dict will be used to store the inverted coordinates and their values
 
-def row_analyzer(values_dict,min_value,max_value,blank_rows_insert=0):
+def row_analyzer(values_dict,min_value,max_value):
 	
 	for rowValue in range(min_value,max_value):
 		
@@ -69,17 +70,73 @@ def row_analyzer(values_dict,min_value,max_value,blank_rows_insert=0):
 			# convert the colValue into a letter coordinate
 			column_letter = get_column_letter(colValue)
 			# combine the column coordinate and row coordinate
-			cell_coordinate = column_letter + str(rowValue + blank_rows_insert)
+			cell_coordinate = column_letter + str(rowValue)
 			# get the cell coordinate's value and push it into the values_list
 			cell_value = sheet[cell_coordinate].value
 
+			# now invert the cell coordinate before storing
+			invert_column_letter = get_column_letter(rowValue) # the row value becomes the column value so get its letter
+			logging.debug('The invert_column_letter is:  %s' % (invert_column_letter) )
+			# create the invert_cell_coordinate
+			invert_cell_coordinate = invert_column_letter + str(colValue) # the column value becomes the row value
+			logging.debug('The invert_cell_coordinate is:  %s' % (invert_cell_coordinate) )
+
+			# store the invert_cell_coordinate as the key, with value as cell_value
 			values_dict[cell_coordinate] = cell_value
 			# logging.debug('The value for %s has been stored in the values_dict' % (cell_coordinate))
 			# logging.debug('The value for %s' % (cell_coordinate) )
 			# logging.debug(cell_value)
 
+
+# +1 because we're not starting at 0
+# do not set the upper limit to upper_row_max+1 as normal, instead set it to n + 1 as we will be inserting the gap at that point
+# we still add + 1 because range() stops 1 point before position_row_N normally, we want it to stop exactly at position_row_N
+
+row_analyzer(inverted_dict,1,upper_row_max + 1)
+
+logging.debug('The inverted dict is:  ')
+logging.debug(inverted_dict)
+
 #####################################
 # END ANALYZE WORKBOOK DATA
+#####################################
+
+#####################################
+# BUILD NEW WORKBOOK
+#####################################
+
+def row_builder(values_dict,workbook):
+
+	sheet = workbook.active
+
+	# for testing
+	# for k,v in values_dict.items():
+	# 	print(k)
+	# 	print(v)
+
+	for k,v in values_dict.items():
+		logging.debug('The key value to use is:  %s' % (k))
+		logging.debug('The value of the key is:  ')
+		logging.debug(v)
+		sheet[k] = v
+
+
+# create new spreadsheet to store values
+
+nwb = openpyxl.Workbook()
+
+# write the values from values_uptoN_list
+
+row_builder(inverted_dict,nwb)
+
+# save the final sheet
+
+nwb.save('invertedSheet.xlsx')
+logging.debug('Spreadsheet file saved.')
+
+
+#####################################
+# END BUILD NEW WORKBOOK
 #####################################
 
 
